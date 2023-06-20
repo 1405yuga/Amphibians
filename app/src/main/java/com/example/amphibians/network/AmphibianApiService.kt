7ed925a4f3fd1aef7f39a85cc9f5d30f8c48e51c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-20 12:49:04 +0530

    created api service interface and lazy object

--- app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -19,6 +19,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 //  Create a property for the base URL provided in the codelab
 private const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/android-basics-kotlin-unit-4-pathway-2-project-api.json"
@@ -28,15 +29,24 @@ val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-// TODO: Build a Retrofit object with the Moshi converter
+//  Build a Retrofit object with the Moshi converter
 val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface AmphibianApiService {
-    // TODO: Declare a suspended function to get the list of amphibians
+    //  Declare a suspended function to get the list of amphibians
+
+    @GET("information")
+    suspend fun getInfo() : List<Amphibian>
 }
 
-// TODO: Create an object that provides a lazy-initialized retrofit service
+//  Create an object that provides a lazy-initialized retrofit service
+
+object AmphibianApi{
+    val retrofitService : AmphibianApiService by lazy{
+        retrofit.create(AmphibianApiService::class.java)
+    }
+}
 
